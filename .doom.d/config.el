d015675c0f8af51747fb08affcc04a907abdd2d5
invokdev <kdempsey@invokdev.com>
2021-06-22 17:11:07 -0400

    updated emacs config and added kitty config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Kevin Dempsey"
+(setq user-full-name "invokdev"
       user-mail-address "kdempsey@invokdev.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
